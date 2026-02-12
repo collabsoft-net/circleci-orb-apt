601d363dcae45c001baffc2fb8c59cb0718deebd
Remie Bolte <remie@collabsoft.net>
2026-02-12 19:38:00 +0100

    fix: permission denied error

--- src/scripts/install-amps.sh
+++ src/scripts/install-amps.sh
@@ -10,7 +10,7 @@ sudo tar -xvzf /tmp/atlassian-plugin-sdk.tar.gz --strip-components 1 -C /opt/atl
 # Hack, hack, hackery hack
 # The sonatype forge repository returns a 402 payment required, so we should disable it.
 # The easiest way to do that is by replacing the URL
-sudo grep -rl "https://repository.sonatype.org/content/repositories/forge/" /opt/atlassian-plugin-sdk | xargs sed -i 's,https://repository.sonatype.org/content/repositories/forge/,https://packages.atlassian.com/artifactory/maven-atlassian-all/,g'
+sudo grep -rl "https://repository.sonatype.org/content/repositories/forge/" /opt/atlassian-plugin-sdk | sudo xargs sed -i 's,https://repository.sonatype.org/content/repositories/forge/,https://packages.atlassian.com/artifactory/maven-atlassian-all/,g'
 
 sudo ln -s /opt/atlassian-plugin-sdk/bin/atlas-mvn /usr/bin/mvn
 sudo mvn --version
